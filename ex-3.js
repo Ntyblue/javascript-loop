7c67ac465021b4d5dde4076fa15387beb574f276
namtan <sirinrat.laoprom@gmail.com>
2025-10-03 00:19:09 +0700

    do exercise1-6

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,13 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+const compareScore = scores[0];
+for (let i = 1; i < scores.length; i++) {
+  if (compareScore > scores[i]) {
+    minScore = scores[i];
+  } else {
+    minScore = minScore;
+  }
+}
 
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,9 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let i = 0; i < restaurants.length; i++) {
+  const rename = `Restaurant Name: ${restaurants[i]}`;
+  newRestaurants.push(rename);
+}
 
 console.log(newRestaurants);
